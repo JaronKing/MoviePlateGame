85b8c4d49725c5e5c1b637f2693b5c06e790d185
JaronKing <jaronjking@gmail.com>
2023-09-24 21:08:29 -0700

    Add shadow to input.

--- src/App.js
+++ src/App.js
@@ -423,7 +423,7 @@ const App = () => {
                 <label htmlFor="large-input" className="text-center block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                     Move Plate Game
                 </label>
-                <input type="text" value={input} onChange={handleInputChange} disabled={movies.isLoading} id="large-input" className="block w-full p-4 text-gray-900 border border-gray-300 bg-white sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 text-center"/>
+                <input type="text" value={input} onChange={handleInputChange} disabled={movies.isLoading} id="large-input" className="block w-full p-4 text-gray-900 border border-gray-300 bg-white sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 text-center shadow-lg"/>
             </div>
 
             <Accordion collapseAll className="px-5 mb-5 mx-5 border border-gray-300 bg-white shadow-lg rounded-none">
