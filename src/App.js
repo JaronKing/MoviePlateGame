1683d73bf1952ff100802a944ddf4263b0a98339
JaronKing <jaronjking@gmail.com>
2023-10-24 12:11:15 -0700

    Title updates.

--- src/App.js
+++ src/App.js
@@ -457,7 +457,7 @@ const App = () => {
 
             <div className="mb-6 pt-10 px-5">
                 <label htmlFor="large-input" className="text-center block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                    Move Plate Game
+                    Movie Plate Game
                 </label>
                 <input type="text" value={input} onChange={handleInputChange} disabled={movies.isLoading} id="large-input" placeholder="Enter Your License Plate" className="block w-full p-4 text-gray-900 border border-gray-300 bg-white sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 text-center shadow-lg"/>
             </div>
